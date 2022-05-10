b7663c63d6bdb6f0cbc7f588b8af2318d258a7a5
Danil Sokolov <danilzyx@gmail.com>
2022-05-10 19:06:25 +0300

    added time constants

--- jamjobbotbackend/utils/extract_datetime.py
+++ jamjobbotbackend/utils/extract_datetime.py
@@ -1,4 +1,7 @@
+import datetime
 from enum import Enum
+from dataclasses import dataclass
+from typing import Optional
 
 
 class Month(Enum):
@@ -26,7 +29,52 @@ class WeekDay(Enum):
     SUNDAY = 7
 
 
-class DateTimeShift(Enum):
+class DayShift(Enum):
     TODAY = 0
     TOMORROW = 1
     DAY_AFTER_TOMORROW = 2
+
+@dataclass
+class DateTimeMarkers:
+    year: Optional[int]
+    time: Optional[datetime.time]
+    day: Optional[WeekDay]
+    month: Optional[Month]
+    day_shift: Optional[DayShift]
+
+
+def extract_datetime_markers(text: str) -> DateTimeMarkers:
+    dayShiftRegexpMap = {
+        'сегодня': DayShift.TODAY,
+        'завтра': DayShift.TOMORROW,
+        'послезавтра': DayShift.DAY_AFTER_TOMORROW
+    }
+
+    weekDayRegexpMap = {
+        'понедельник|пн': WeekDay.MONDAY,
+        'вторник|вт': WeekDay.TUESDAY,
+        'сред|ср': WeekDay.WEDNESDAY,
+        'четверг|чт': WeekDay.THURSDAY,
+        'пятниц|пт': WeekDay.FRIDAY,
+        'субот|сб': WeekDay.SATURDAY,
+        'воскресен|вс': WeekDay.SUNDAY,
+    }
+
+    monthRegexpMap = {
+        'январь': Month.JANUARY,
+        'февраль': Month.FEBRUARY,
+        'март': Month.MARCH,
+        'апрель': Month.APRIL,
+        'май': Month.MAY,
+        'июнь': Month.JUNE,
+        'июль': Month.JULY,
+        'август': Month.AUGUST,
+        'сентябрь': Month.SEPTEMBER,
+        'октябрь': Month.OCTOBER,
+        'ноябрь': Month.NOVEMBER,
+        'декабрь': Month.DECEMBER,
+    }
+
+
+def normalize_datetime_markers(markers: DateTimeMarkers, current_datetime: datetime.datetime) -> datetime.datetime:
+    pass
